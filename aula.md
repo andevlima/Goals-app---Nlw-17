5f48cfc651abe1dc44b4ce2024b072dd82e75e0f
andr3lima <github_andr3lima@users.noreply.github.com>
2024-09-12 08:07:38 -0300

    fundamentals 004 + application menu

--- aula.md
+++ aula.md
@@ -93,3 +93,49 @@ const start = () => {
 }
 
 start()
+
+.........................
+
+// menu da aplicação
+// selecionando um prompt da package instalada (inquirer)
+const { select } = require("@inquirer/prompts")
+
+// sempre que usar await, a função tem q ter um async
+const start = async () => {
+  while (true) {
+    // o await serve pra esperar a pessoa selecionar alguma opção pra poder rodar a função
+    const opcao = await select({
+      // na seleção do usuário serão mostradas a mensagem e as opções abaixo
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "cadastrar",
+        },
+        {
+          name: "Listar metas",
+          value: "listar",
+        },
+        {
+          name: "Sair",
+          value: "sair",
+        },
+      ],
+    })
+
+    // o valor da opção escolhida em choices será guardado em 'opcao', switch recebe 'opcao' e executa conforme o código abaixo
+    switch (opcao) {
+      case "cadastrar":
+        console.log("vamos cadastrar")
+        break
+      case "listar":
+        console.log("vamos listar")
+        break
+      case "sair":
+        console.log("Até a próxima!")
+        return
+    }
+  }
+}
+
+start()
--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+// selecionando um prompt da package instalada (inquirer)
+const { select } = require("@inquirer/prompts")
 
-const start = () => {
-  while(count < 10){
-    let opcao = "sair"
-    switch(opcao) {
+// sempre que usar await, a função tem q ter um async
+const start = async () => {
+  while (true) {
+    // o await serve pra esperar a pessoa selecionar alguma opção pra poder rodar a função
+    const opcao = await select({
+      // na seleção do usuário serão mostradas a mensagem e as opções abaixo
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "cadastrar",
+        },
+        {
+          name: "Listar metas",
+          value: "listar",
+        },
+        {
+          name: "Sair",
+          value: "sair",
+        },
+      ],
+    })
+
+    // o valor da opção escolhida em choices será guardado em 'opcao', switch recebe 'opcao' e executa conforme o código abaixo
+    switch (opcao) {
       case "cadastrar":
         console.log("vamos cadastrar")
         break
@@ -10,7 +33,8 @@ const start = () => {
         console.log("vamos listar")
         break
       case "sair":
-        return    
+        console.log("Até a próxima!")
+        return
     }
   }
 }
