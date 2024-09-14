ecb9324bdb0ba53e5e0b0337ce85fa4e302645f1
andr3lima <github_andr3lima@users.noreply.github.com>
2024-09-14 11:14:52 -0300

    persist data

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 // selecionando um prompt da package instalada (inquirer)
 const { select, input, checkbox } = require("@inquirer/prompts")
+const fs = require("fs").promises
 
 let mensagem = "Bem vindo ao App de Metas!";
 
-let meta = {
-  value: "Tomar 3L de água por dia",
-  checked: false,
+let metas
+
+// parse transforma JSON em JS
+const carregarMetas = async () => {
+  try {
+    const dados = await fs.readFile("metas.json", "utf-8")
+    metas = JSON.parse(dados)
+  }
+  catch(erro) {
+    metas = []
+  }
 }
 
-let metas = [meta]
+// stringfy transforma JS em JSON
+const salvarMetas = async () => {
+  await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
 
 const cadastrarMeta = async () => {
   const meta = await input({ message: "Digite a meta:" })
@@ -26,6 +38,10 @@ const cadastrarMeta = async () => {
 }
 
 const listarMetas = async () => {
+  if(metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
   const respostas = await checkbox({
     message:
       "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
@@ -71,6 +87,11 @@ const metasRealizadas = async () => {
 
 // aqui entra as metas q não foram marcadas
 const metasAbertas = async () => {
+  if (metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
+
   const abertas = metas.filter((meta) => {
     return meta.checked != true // != sinal de diferente
   })
@@ -87,6 +108,11 @@ const metasAbertas = async () => {
 }
 
 const deletarMetas = async () => {
+  if (metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
+  
   const metasDesmarcadas = metas.map((meta) => {
     return { value: meta.value, checked: false }
   })
@@ -123,8 +149,11 @@ const mostrarMensagem = () => {
 
 // sempre que usar await, a função tem q ter um async
 const start = async () => {
+  await carregarMetas()
+
   while (true) {
     mostrarMensagem()
+    await salvarMetas()
     // o await serve pra esperar a pessoa selecionar alguma opção pra poder rodar a função
     const opcao = await select({
       // na seleção do usuário serão mostradas a mensagem e as opções abaixo
